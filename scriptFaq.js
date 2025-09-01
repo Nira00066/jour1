d27936fb859dbe1fddc42b28850aa004b8a10744
jefflearning40 <jfld791000@gmail.com>
2025-09-01 14:03:47 +0200

    refacto script

--- scriptFaq.js
+++ scriptFaq.js
@@ -1,10 +1,24 @@
 // Sélectionner tous les accordion-items
-const accordionItem = document.querySelectorAll(".accordion-item");
+const accordionItems = document.querySelectorAll(".accordion-item");
 
 // Pour chaque item
-accordionItem.forEach(item => {
-    const header = item.querySelector(".accordion-header"); // ✅ corrigé ici
+accordionItems.forEach(item => {
+    const header = item.querySelector(".accordion-header");
+
     header.addEventListener("click", () => {
+        // quand j'ouvre un iteme je Ferme tous les autres 
+        //avec foreach, je parcours tous les accordions-item
+        //et otherItem est un element dans la boucle foreach
+        accordionItems.forEach(otherItem => {
+            //si other item n'est pas le meme que celui que je clique alors on ignore celui qu'on vient d'ouvrir et on ferme tous les autres
+            if (otherItem !== item) {
+                //fermer les autres c'est retirer active
+                otherItem.classList.remove("active");
+                //Ferme toutes les autres questions sauf celle que je viens de cliquer
+            }
+        });
+
+        // Ouvrir/fermer l'item cliqué, c'est le principe du toggle utilisé egalement en electronique pour un interrupteur      (on1-off-on2).
         item.classList.toggle("active");
     });
 });
